1c1be7b109a4b2536934b7cc7453d35d543aa093
ThomasMujica <temc.business@gmail.com>
2023-12-05 15:56:02 -0300

    Finalizando 1.5: información del curso paso 5

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.name}</h1>
     </>
   );
 };
@@ -37,28 +37,31 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      Number of exercises .................... <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      Number of exercises ....................
+      <Total parts={course.parts} />
     </div>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
