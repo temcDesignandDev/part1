07757923f066276a368f85eb8bbece07f55cc577
ThomasMujica <temc.business@gmail.com>
2023-12-08 20:29:02 -0300

    Finalizando 1.6: unicafe, paso 1

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodClick = () => {
+    console.log("Good clickeado");
+    setGood(good + 1);
+  };
+
+  const neutralClick = () => {
+    console.log("Neutral clickeado");
+    setNeutral(neutral + 1);
+  };
+
+  const badClick = () => {
+    console.log("Bad clickeado");
+    setBad(bad + 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Give FeedBack</h1>
+      <button onClick={goodClick}>Good</button>
+      <button onClick={neutralClick}>Neutral</button>
+      <button onClick={badClick}>Bad</button>
+      <h2>Statistic</h2>
+      <p>Good {good}</p>
+      <p>Neutal {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
